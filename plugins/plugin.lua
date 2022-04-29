1fb3413534c99a4017625b2a7f9cad6e8775cf0f
Piotr Wyrwas <pw.piotr.wyrwas@gmail.com>
2022-04-29 18:54:20 +0200

    Remove LUA related stuff [for now]

--- plugins/plugin.lua
+++
@@ -1,3 +0,0 @@
-local function run()
-
-end
--- src/ufs.h
+++
@@ -1,9 +0,0 @@
-// File System Utility functions
-
-#ifndef TGXP_UFS_H
-#define TGXP_UFS_H
-
-int TGXP_DirectoryExists(char *);
-int TGXP_CreateDirectory(char *);
-
-#endif
